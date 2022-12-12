1a4469786f2c1ec803953917692b5ca8973758e4
Kamil Graczyk <k.d.graczyk@gmail.com>
2022-12-12 16:34:57 +0100

    return input id

--- src/components/islands/Table.tsx
+++ src/components/islands/Table.tsx
@@ -24,8 +24,9 @@ const Table: FunctionalComponent = () => {
                 <label
                 for="default-search"
                 className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
-                >Pole wyszykiwania</label
                 >
+                    Pole wyszykiwania
+                </label>
                 <div className="relative">
                 <div
                     className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none"
@@ -51,6 +52,7 @@ const Table: FunctionalComponent = () => {
                     value={search}
                     type="search"
                     name="search"
+                    id="default-search"
                     className="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                     placeholder="Wpisz model telefonu lub szukaną usterkę..."
                 />
