338afcdc751e4b4218ab76668fc452120993aabc
Kamil Graczyk <k.d.graczyk@gmail.com>
2022-12-16 11:53:09 +0100

    update astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,9 +9,10 @@ import partytown from '@astrojs/partytown'
 import preact from '@astrojs/preact'
 import { SITE } from './src/config.mjs'
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
+const __site = import.meta.env.PROD ? 'https://ihave.gsm.pl' : SITE.origin
 
 export default defineConfig({
-  site: SITE.origin,
+  site: __site,
   base: SITE.basePathname,
   trailingSlash: SITE.trailingSlash ? 'always' : 'never',
   output: 'static',
